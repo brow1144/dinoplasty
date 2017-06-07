a86b9d21979d1b68608ffa52d51b57721479a367
Kyle Brown <brow1144@purdue.edu>
2017-06-07 16:06:52 -0400

    Add event listener for dino form

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('HI')
+const app = {
+
+    init(formSelector) {
+        document
+            .querySelector(formSelector)
+            .addEventListener('submit', this.addDino)
+    },
+
+    addDino(e) {
+        e.preventDefault()
+        const dino = e.target.dinoName.value
+        console.log(dino)
+    },
+
+}
+
+app.init('#dino-form')
+
+
